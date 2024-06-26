dd79481ecac4dfe6c73662c49cc50757304ab836
Okhrimenko Mykhailo <misha14122002@gmail.com>
2024-06-26 17:36:35 +0300

    +

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using PersonaTest.Infrastucture;
+using PersonaTest.Infrastucture.Repositories.Interfaces;
+using PersonaTest.Infrastucture.Repositories;
+using PersonaTest.Core.Services.Interfaces;
+using PersonaTest.Core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,11 @@ builder.Services.AddSwaggerGen();
 
 var connection = builder.Configuration.GetConnectionString("ConnectionString:PersonaDB");
 builder.Services.AddDbContext<PersonaTestContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IPersonaRepository, PersonaRepository>();
+builder.Services.AddScoped<IPersonaService, PersonaService>();
+
 
 var app = builder.Build();
 
